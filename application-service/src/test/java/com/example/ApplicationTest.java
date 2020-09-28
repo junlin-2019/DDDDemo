80addb8998b9f003d2b0cfe70a4162dc3e602ae5
hehang <hehang@fang.com>
2020-09-28 10:08:51 +0800

    添加测试类

---
+++ application-service/src/test/java/com/example/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description:
+ * @Author: admin
+ * @Date: 2020/9/28 10:07
+ */
+@SpringBootTest
+public class ApplicationTest {
+
+
+}
